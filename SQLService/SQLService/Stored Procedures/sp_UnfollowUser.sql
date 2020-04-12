628fe99edc37e7083b50053d6674f10c49dab2d2
pawan kumar <pawkumar@microsoft.com>
2020-04-13 01:08:43 +0530

    Unfollow user/question/user and added kept track of user giving the answer to a question.

---
+++ SQLService/SQLService/Stored Procedures/sp_UnfollowUser.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[sp_UnfollowUser]      
+(   
+	@unfollower_user_id BIGINT,
+    @unfollowed_user_id BIGINT
+)      
+AS
+BEGIN  
+    SET NOCOUNT ON; 
+    BEGIN TRY
+        BEGIN TRAN
+            DELETE FROM [dbo].[follow_user] where follower_user_id = @unfollower_user_id and followed_user_id = @unfollower_user_id;
+        COMMIT TRAN
+    END TRY
+    BEGIN CATCH
+        IF(@@TRANCOUNT > 0)
+            ROLLBACK TRAN;
+        THROW; 
+    END CATCH
+END
---
+++ SQLService/SQLService/Tables/user_gave_kudos.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[user_gave_kudos]
+(
+	[user_id] BIGINT NOT NULL , 
+    [kudos_answer_id] BIGINT NOT NULL, 
+    [kudos_question_id] BIGINT NOT NULL,
+    PRIMARY KEY ([user_id]),
+    FOREIGN KEY (user_id) REFERENCES [dbo].[users] (user_id)
+)
