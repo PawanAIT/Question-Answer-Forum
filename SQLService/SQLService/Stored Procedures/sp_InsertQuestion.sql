a0367fe1fe30859f27bf623e1960c8e63fcda4ea
pawan kumar <pawkumar@microsoft.com>
2020-04-12 23:12:31 +0530

    Upvote and downvote added and question seen.

--- SQLService/SQLService/Stored Procedures/sp_InsertQuestion.sql
+++ SQLService/SQLService/Stored Procedures/sp_InsertQuestion.sql
@@ -1,4 +1,4 @@
-﻿CREATE PROCEDURE [dbo].[sp_InsertQuestions]      
+﻿CREATE PROCEDURE [dbo].[sp_InsertQuestion]      
 (      
     @question_title NVARCHAR (1000),
     @question_details NVARCHAR (4000),
---
+++ SQLService/SQLService/Tables/UserSeenQuestion.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[user_seen_Question]
+(
+	[user_id] BIGINT NOT NULL , 
+    [seen_question_id] BIGINT NOT NULL, 
+    PRIMARY KEY ([user_id], [seen_question_id]),
+    FOREIGN KEY (user_id) REFERENCES [dbo].[users] (user_id)
+)
