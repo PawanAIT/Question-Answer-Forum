3120726ab9582818a5e0854475372eb8ffb13e84
pawan kumar <pawkumar@microsoft.com>
2020-04-19 22:27:35 +0530

    Working Oauth Flow and dropping Oauth Token to server

--- GOSQLServices/src/Client/Client.go
+++ GOSQLServices/src/Client/Client.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"Client_interceptor"
+	"fmt"
 	"log"
 	"net/http"
 	"proto"
 
 	"github.com/gin-gonic/gin"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 	"google.golang.org/grpc"
 )
 
@@ -18,7 +21,24 @@ func authMethods() map[string]bool {
 	}
 }
 
+var (
+	googleOauthConfig *oauth2.Config
+	// TODO: randomize it
+	oauthStateString = "pseudo-random"
+)
+
+func init() {
+	googleOauthConfig = &oauth2.Config{
+		RedirectURL:  "http://localhost:8080/callback",
+		ClientID:     "392376614027-qpuh3t81joaucdt4kcgpq84gbfqrr83f.apps.googleusercontent.com",
+		ClientSecret: "0ISYm6CUhkyezn_JpBzV1lIA",
+		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
+		Endpoint:     google.Endpoint,
+	}
+}
+
 func main() {
+
 	interceptor, err := Client_interceptor.NewAuthInterceptor(authMethods())
 	if err != nil {
 		panic(err)
@@ -26,6 +46,17 @@ func main() {
 	conn, err := grpc.Dial("localhost:4040", grpc.WithInsecure(), grpc.WithUnaryInterceptor(interceptor.Unary()))
 	Client := proto.NewAddServiceClient(conn)
 	g := gin.Default()
+	g.GET("/", func(ctx *gin.Context) {
+		var w http.ResponseWriter = ctx.Writer
+		var r *http.Request = ctx.Request
+		handleMain(w, r)
+	})
+	g.GET("/login", func(ctx *gin.Context) {
+		var w http.ResponseWriter = ctx.Writer
+		var r *http.Request = ctx.Request
+		handleGoogleLogin(w, r)
+	})
+
 	g.GET("/add/:a/:b/:c", func(ctx *gin.Context) {
 
 		req := &proto.User{FirstName: ctx.Param("a"), LastName: ctx.Param("b"), Email: ctx.Param("c")}
@@ -54,4 +85,36 @@ func main() {
 		log.Fatalf("Failed to run server: %v", err)
 	}
 
+}
+
+//OAuthInfo contains data returned from OAuth2 Verifier
+type OAuthInfo struct {
+	ID            string `json:"id"`
+	Email         string `json:"email"`
+	VerifiedEmail bool   `json:"verified_email"`
+	Picture       string `json:"picture"`
+}
+
+func init() {
+	googleOauthConfig = &oauth2.Config{
+		RedirectURL:  "http://localhost:4040/callback",
+		ClientID:     "392376614027-qpuh3t81joaucdt4kcgpq84gbfqrr83f.apps.googleusercontent.com",
+		ClientSecret: "0ISYm6CUhkyezn_JpBzV1lIA",
+		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
+		Endpoint:     google.Endpoint,
+	}
+}
+
+func handleMain(w http.ResponseWriter, req *http.Request) {
+	var htmlIndex = `<html>
+	<body>
+		<a href="/login">Google Log In</a>
+	</body>
+	</html>`
+
+	fmt.Fprintf(w, htmlIndex)
+}
+func handleGoogleLogin(w http.ResponseWriter, r *http.Request) {
+	url := googleOauthConfig.AuthCodeURL(oauthStateString)
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
 }
--- GOSQLServices/src/Server/Server.go
+++ GOSQLServices/src/Server/Server.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"Server_interceptor"
+	"auth"
 	"context"
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
+	"net/http"
+	"time"
 
 	"msdb"
 
 	"proto"
 
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
@@ -19,7 +26,11 @@ import (
 type server struct{}
 
 func main() {
-	listener, err := net.Listen("tcp", ":4040")
+	listener, err := net.Listen("tcp", ":4041")
+	http.HandleFunc("/", handleMain)
+	http.HandleFunc("/callback", handleGoogleCallback)
+
+	http.ListenAndServe(":4040", nil)
 
 	if err != nil {
 		panic(err)
@@ -37,22 +48,91 @@ func main() {
 
 }
 
-/*
-func (s *server) Add(ctx context.Context, request *proto.Request) (*proto.Response, error) {
-	a, b := request.GetA(), request.GetB()
+func handleMain(w http.ResponseWriter, r *http.Request) {
+	var htmlIndex = `<html>
+<body>
+	<a href="/login">Google Log In</a>
+</body>
+</html>`
 
-	result := a + b
-
-	return &proto.Response{Result: result}, nil
+	fmt.Fprintf(w, htmlIndex)
 }
 
-func (s *server) Multiply(ctx context.Context, request *proto.Request) (*proto.Response, error) {
-	a, b := request.GetA(), request.GetB()
+type OAuthInfo struct {
+	ID            string `json:"id"`
+	Email         string `json:"email"`
+	VerifiedEmail bool   `json:"verified_email"`
+	Picture       string `json:"picture"`
+}
 
-	result := a * b
+func init() {
+	googleOauthConfig = &oauth2.Config{
+		RedirectURL:  "http://localhost:4040/callback",
+		ClientID:     "392376614027-qpuh3t81joaucdt4kcgpq84gbfqrr83f.apps.googleusercontent.com",
+		ClientSecret: "0ISYm6CUhkyezn_JpBzV1lIA",
+		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
+		Endpoint:     google.Endpoint,
+	}
+}
 
-	return &proto.Response{Result: result}, nil
-}*/
+func handleGoogleCallback(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Inside callback")
+	userInfo, err := getUserInfo(r.FormValue("state"), r.FormValue("code"))
+	if err != nil {
+		fmt.Println(err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	fmt.Println(userInfo.Email)
+
+	jwt := auth.NewJWTManager("foo", time.Minute*5)
+	token, err := jwt.Generate(&auth.User{
+		Username: userInfo.Email,
+		Id:       userInfo.ID,
+	})
+	if err != nil {
+		fmt.Println("Unable to generate token")
+	}
+	fmt.Println(token)
+}
+
+var (
+	googleOauthConfig *oauth2.Config
+	// TODO: randomize it
+	oauthStateString = "pseudo-random"
+)
+
+func getUserInfo(state string, code string) (*OAuthInfo, error) {
+	if state != oauthStateString {
+		return nil, fmt.Errorf("invalid oauth state")
+	}
+
+	fmt.Println(code)
+
+	token, err := googleOauthConfig.Exchange(oauth2.NoContext, code)
+
+	if err != nil {
+		return nil, fmt.Errorf("code exchange failed: %s", err.Error())
+	}
+
+	response, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + token.AccessToken)
+	if err != nil {
+		return nil, fmt.Errorf("failed getting user info: %s", err.Error())
+	}
+	defer response.Body.Close()
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed reading response body: %s", err.Error())
+	}
+
+	var userInfo OAuthInfo
+	err = json.Unmarshal(contents, &userInfo)
+	if err != nil {
+		panic(err)
+	}
+	return &userInfo, nil
+}
 
 func (s *server) AddUser(ctx context.Context, request *proto.User) (*proto.Response, error) {
 	fName, lName, email := request.GetFirstName(), request.GetLastName(), request.GetEmail()
